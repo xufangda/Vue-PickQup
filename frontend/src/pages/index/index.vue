173192b684d389b6d4624f2a92db1a61a3f2eae6
fangda <poi998998@gmail.com>
2018-04-22 20:55:07 +0100

    init

---
+++ frontend/src/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <index-header></index-header>
+    <index-swiper></index-swiper>
+  </div>
+</template>
+
+<script>
+import IndexHeader from './components/header'
+import IndexSwiper from './components/swiper'
+export default {
+  name: 'index',
+  components: {
+    IndexHeader,
+    IndexSwiper
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
